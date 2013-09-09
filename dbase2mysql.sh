14c286ac8ba12b3407565fc752c1573a448f2f4c
Junior Holowka <junior.holowka@gmail.com>
2013-09-09 17:59:20 -0300

    Updated

--- dbase2mysql.sh
+++ dbase2mysql.sh
@@ -1,11 +1,30 @@
 #!/bin/bash -e
 #
-# dbase2mysql - Migrate DBF files to MySql database
+# dbase2mysql.sh - Migrate DBF files to MySql database
 #
-# Copyright (c) 2012 Junior Holowka <junior.holowka@gmail.com>
+# Copyright (c) 2013 Junior Holowka <junior.holowka@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 #
 # HOWTO: ./dbase2mysql.sh - This script must be run at the same *.dbf directory
-#
+
+
 
 HOST="localhost"
 USER="root"
@@ -28,7 +47,7 @@ do
 	FILEXTEN="$(echo $FILENAME | sed 's/\..\{3\}$//')"
 
 	echo -e "\033[1m===> Migrating $FILENAME to $HOST>$BASE>$FILEXTEN ... \033[0m\n"
-	dbf2mysql -h $HOST -P $PASS -U $USER -d $BASE -t $FILEXTEN -c $FILENAME
+	dbf2mysql -h $HOST -P $PASS -U $USER -d $BASE -t $FILEXTEN -c $FILENAME	
 
 done
 exit 0
